0b07ff45d29b981150d87abd02dfad0edea920c9
Edmarc Sapa <marcgsapa@gmail.com>
2024-07-29 13:59:27 -0700

    Initial fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./styles/styles.scss";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Navigation from "./components/Navigation";
 import SubNav from "./components/SubNav";
 
 function App() {
@@ -12,26 +11,7 @@ function App() {
     <>
       <Header />
       <Hero />
-      <Navigation />
       <SubNav />
-
-      <h1>React Movie Project</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR Add a comment here
-          to do initial push
-          {/* please do "git checkout -b "yourname-branch" without the quotation marks */}
-          {/* after creating your own branch, please add a comment here just like mine, save, then do a commit named "initial commit -yourname"
-          then push the commit and merge with branch main*/}
-          {/* please be sure to inform the group chat once its done */}
-          {/* Initial Push - Marc */}
-          {/* Initial Push - Yining */}
-          <p>Hello World!</p>
-        </p>
-      </div>
     </>
   );
 }
