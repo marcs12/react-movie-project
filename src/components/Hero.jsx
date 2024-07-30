4557bb034ac456476bdd0a2ee4cd1885ae197d1b
Edmarc Sapa <marcgsapa@gmail.com>
2024-07-30 11:55:33 -0700

    Hero API Fetch

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { moviePoster, apiKey, API, movieAPI } from "../globals/globalVariables";
 
 const Hero = () => {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(movieAPI);
+      const data = await response.json();
+      console.log(data);
+      setMovie(data.results);
+    };
+
+    fetchData();
+  }, [setMovie]);
+
+  // console log the movie poster from the array of objects
+  console.log(movie.map((Val) => Val.poster_path));
+
   return (
     <>
       <section className="hero-section">
