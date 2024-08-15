e5b361634270533ffbd2018ec9b9d7c50eb32e7d
Edmarc Sapa <marcgsapa@gmail.com>
2024-08-15 12:01:38 -0700

    removed Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,15 +19,7 @@ const Hero = () => {
 
   return (
     <>
-      <section className="hero-section">
-        <article className="hero-img">
-          <div className="thumbnail"></div>
-          <div className="title">
-            <h2>Placeholder</h2>
-            <p>Description</p>
-          </div>
-        </article>
-      </section>
+      <section className="hero-section"></section>
     </>
   );
 };
