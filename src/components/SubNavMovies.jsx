f1a9394733aac02c4e87bee5abe25f6f078ab463
Edmarc Sapa <marcgsapa@gmail.com>
2024-08-16 13:39:37 -0700

    Fixing Conflicts

--- src/components/SubNavMovies.jsx
+++ src/components/SubNavMovies.jsx
@@ -1,25 +1,24 @@
 import { useEffect, useState } from "react";
 const endpoint = "https://api.themoviedb.org/3/movie/";
 const baseImgURL = "https://image.tmdb.org/t/p/w500/";
-// import { BrowserRouter, Link } from "react-router-dom";
 import { BrowserRouter, Link } from "react-router-dom";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import "../styles/components/_thumbnails.scss";
+>>>>>>> staging
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 
 const API = import.meta.env.VITE_MOVIE_API_KEY;
 
 const Home = () => {
   const [category, setCategory] = useState("now_playing");
   const [movies, setMovies] = useState([]);
-  const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState([]); // State for favorites
 
   useEffect(() => {
     const getMovies = async () => {
       const response = await fetch(`${endpoint}${category}?api_key=${API}`);
       const json = await response.json();
-      setMovies(json.results || []);
+      setMovies(json.results || []); // Ensure movies is an array
     };
 
     getMovies();
@@ -29,10 +28,13 @@ const Home = () => {
     setCategory(id);
   }
 
+  // Function to handle favorite toggle
   const toggleFavorite = (movie) => {
     if (favorites.includes(movie.id)) {
+      // Remove from favorites
       setFavorites(favorites.filter((fav) => fav !== movie.id));
     } else {
+      // Add to favorites
       setFavorites([...favorites, movie.id]);
     }
   };
@@ -81,28 +83,28 @@ const Home = () => {
 
             return (
               <>
-                {/* <BrowserRouter> */}
-                <li key={movie.id} className="movie-wrap">
-                  {/* <Link to={`/movie/${movie.id}`}> */}
-                  <img src={`${baseImgURL}/${movie.poster_path}`} />
-                  {/* </Link> */}
-                  <div className="stars">
-                    <span
-                      className={`star ${isFavorite ? "favorite" : ""}`}
-                      onClick={() => toggleFavorite(movie)}
-                    >
-                      {/* <FontAwesomeIcon
+                <BrowserRouter>
+                  <li key={movie.id} className="movie-wrap">
+                    <Link to={`/movie/${movie.id}`}>
+                      <img src={`${baseImgURL}/${movie.poster_path}`} />
+                    </Link>
+                    <div className="stars">
+                      <span
+                        className={`star ${isFavorite ? "favorite" : ""}`}
+                        onClick={() => toggleFavorite(movie)}
+                      >
+                        {/* <FontAwesomeIcon
                         icon="fa-light fa-star"
                         style={{ color: "#FFD43B" }}
                       /> */}
-                    </span>
-                  </div>
-                  {/* <Link to={`/movies/${movie.id}`}> */}
-                  <div>{movie.title}</div>
-                  <div>{movie.release_date}</div>
-                  {/* </Link> */}
-                </li>
-                {/* </BrowserRouter> */}
+                      </span>
+                    </div>
+                    <Link to={`/movies/${movie.id}`}>
+                      <div>{movie.title}</div>
+                      <div>{movie.release_date}</div>
+                    </Link>
+                  </li>
+                </BrowserRouter>
               </>
             );
           })}
