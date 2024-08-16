c3bc0f40dfda5f795381cf045e8bc6a2a97ec591
ynlidev <ynlidev@gmail.com>
2024-08-16 15:04:17 -0700

    Edited detailspage.jsx.

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import { useParams } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
+
+const endpoint = "https://api.themoviedb.org/3/movie/";
+const baseImgURL = "https://image.tmdb.org/t/p/w500/";
+const API = import.meta.env.VITE_MOVIE_API_KEY;
 
 const Details = () => {
   let { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const response = await fetch(`${endpoint}${id}?api_key=${API}`);
+        const data = await response.json();
+        setMovie(data);
+      } catch (error) {
+        console.error("Error fetching the movie:", error);
+      }
+    };
+
+    fetchMovie();
+  }, [id]);
+
+  if (!movie) return <p>Loading...</p>;
+
   return (
-    
     <>
-    <Header />
+      <Header />
       <section className="">
         <article className="">
-          <img src="" alt="" />
-          <p class= "hero-section">{id}</p>
-
+          <img src={`${baseImgURL}${movie.poster_path}`} alt={movie.title} />
+          <p className="hero-section">{movie.title}</p>
+          <Link to={`/movie/${movie.id}`}>
+            <div>{movie.title}</div>
+            <div>{movie.release_date}</div>
+          </Link>
         </article>
       </section>
     </>
