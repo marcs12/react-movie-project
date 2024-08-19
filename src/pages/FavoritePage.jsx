0859152f51358c77dfddc01b911ee4b6819f8b75
Kate Shepherd <kateshepherd14@my.bcit.ca>
2024-08-19 14:09:46 -0700

    include infinite scroll logic on carousel

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,46 +1,63 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
+import { Carousel } from "react-bootstrap";
 import Header from "../components/Header";
 import Favorites from "../globals/Favorites";
 import { Link } from "react-router-dom";
-import { Carousel } from "react-bootstrap";
 const baseImgURL = "https://image.tmdb.org/t/p/w500/";
 import starSolid from "../assets/Images/star-solid.svg";
 import starRegular from "../assets/Images/star-regular.svg";
 
 const Favorite = () => {
   const { favorites, setFavorites } = useContext(Favorites);
+  const carouselRef = useRef(null);
 
-  // Function to handle favorite toggle
   const toggleFavorite = (movie) => {
     if (favorites.some((obj) => obj.id === movie.id)) {
-      // Remove from favorites
       setFavorites(favorites.filter((fav) => fav.id !== movie.id));
     } else {
-      // Add to favorites
       setFavorites([...favorites, movie]);
     }
   };
 
+  // Infinite scroll effect
+  useEffect(() => {
+    const carousel = carouselRef.current;
+
+    // Handle "slide" event
+    const handleSlideEnd = () => {
+      const activeIndex = carousel.querySelector('.carousel-item.active').dataset.index;
+      if (parseInt(activeIndex) === favorites.length - 1) {
+        setTimeout(() => {
+          // Manually reset to the first slide with no animation
+          carousel.classList.remove('slide');
+          carousel.querySelector('.carousel-item.active').classList.remove('active');
+          carousel.querySelector('.carousel-item').classList.add('active');
+        }, 500); // Timing depends on your slide interval
+      }
+    };
+
+    // Listen for Bootstrap carousel "slid" event
+    carousel.addEventListener('slid.bs.carousel', handleSlideEnd);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      carousel.removeEventListener('slid.bs.carousel', handleSlideEnd);
+    };
+  }, [favorites]);
+
   return (
     <>
       <Header />
-      <section className="">
-        <article className="">
-          <img src="" alt="" />
-          <p className="hero-section">favourites</p>
-        </article>
-      </section>
-
-      <section>
+      <section className="favorite-carousel">
         {favorites.length === 0 ? (
           <p>No favorite movies selected.</p>
         ) : (
-          <Carousel>
-            {favorites.map((movie) => {
+          <Carousel ref={carouselRef} interval={3000} ride="carousel">
+            {favorites.map((movie, index) => {
               const isFavorite = favorites.some((obj) => obj.id === movie.id);
 
               return (
-                <Carousel.Item key={movie.id}>
+                <Carousel.Item key={movie.id} data-index={index}>
                   <Link to={`/movie/${movie.id}`}>
                     <img
                       className="d-block w-100"
@@ -77,4 +94,5 @@ const Favorite = () => {
 };
 
 export default Favorite;
+
 
