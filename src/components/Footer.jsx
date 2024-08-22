f82e1dd4d4e2edf96bc3a09054c8d345fe260533
Kate Shepherd <kateshepherd14@my.bcit.ca>
2024-08-22 16:29:42 -0700

    added footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
+import mainLogo from "../assets/main-logo.png";
+import './footer.scss';
 
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright © FWD 2024 Yining Mahdi Marc and Kate</p>
+        <div className="footer-container">
+            <Link to="/">
+                <img src={mainLogo} alt="35mm Logo" />
+            </Link>
+            <ul className="footer-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/favorites">Favourites</Link></li>
+            </ul>
+            <p>Copyright © FWD 2024 Yining Li Mahdi Roozbahani Marc Sapa Kate Shepherd</p>
+        </div>
     </footer>
   );
 };
 
 export default Footer;
+
