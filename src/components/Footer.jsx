adcfcc98d08f77a7926ae0a34879e488491c268f
Kate Shepherd <kateshepherd14@my.bcit.ca>
2024-08-22 15:17:52 -0700

    created footer component, linked to home page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright © FWD 2024 Yining Mahdi Marc and Kate</p>
+    </footer>
+  );
+};
+
+export default Footer;
