428c5ff1f1f80458af8b41062781119f306d20ac
Kate Shepherd <kateshepherd14@my.bcit.ca>
2024-08-23 15:27:22 -0700

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import mainLogo from "../assets/main-logo.png";
+
 
 const Footer = () => {
   return (
     <footer>
-        <div className="footer-container">
-            {/* <ul className="footer-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/favorites">Favourites</Link></li>
-            </ul> */}
-            {/* <ul className="sub-menulinks">
-            <li><Link to="/now_playing">Now Playing</Link></li>
-            <li><Link to="/top_rated">Top Rated</Link></li>
-            <li><Link to="/upcoming">Upcoming</Link></li>
-            </ul> */}
-            <Link to="/">
+       <Link to="/">
             <span class="line"></span>
                 <img src={mainLogo} alt="35mm Logo" />
                 <span class="line"></span>
             </Link>
-            {/* <p>Copyright © FWD 2024 Yining Li Mahdi Roozbahani Marc Sapa Kate Shepherd</p> */}
+        <div className="footer-container">
+            <ul className="footer-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/favorites">Favourites</Link></li>
+            </ul>
+            <ul className="sub-menulinks">
+            <li><Link to="/now_playing">Now Playing</Link></li>
+            <li><Link to="/top_rated">Top Rated</Link></li>
+            <li><Link to="/upcoming">Upcoming</Link></li>
+            </ul>
+          
+            <p>Copyright © FWD 2024 Yining Li Mahdi Roozbahani Marc Sapa Kate Shepherd</p>
         </div>
     </footer>
   );
 };
 
 export default Footer;
-
