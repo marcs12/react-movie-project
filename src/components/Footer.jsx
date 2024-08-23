e3ad0e2dbf68cf6deeb81edafe55f8544c9ec66b
Kate Shepherd <kateshepherd14@my.bcit.ca>
2024-08-22 17:16:38 -0700

    added footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 import mainLogo from "../assets/main-logo.png";
-import './footer.scss';
 
 const Footer = () => {
   return (
     <footer>
         <div className="footer-container">
-            <Link to="/">
-                <img src={mainLogo} alt="35mm Logo" />
-            </Link>
-            <ul className="footer-links">
+            {/* <ul className="footer-links">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/favorites">Favourites</Link></li>
-            </ul>
-            <p>Copyright © FWD 2024 Yining Li Mahdi Roozbahani Marc Sapa Kate Shepherd</p>
+            </ul> */}
+            <Link to="/">
+            <span class="line"></span>
+                <img src={mainLogo} alt="35mm Logo" />
+                <span class="line"></span>
+            </Link>
+            {/* <p>Copyright © FWD 2024 Yining Li Mahdi Roozbahani Marc Sapa Kate Shepherd</p> */}
         </div>
     </footer>
   );
