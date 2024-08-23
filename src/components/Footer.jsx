90bccc67fb1f930c2a72990251817fbb1eac3deb
Kate Shepherd <kateshepherd14@my.bcit.ca>
2024-08-23 16:11:19 -0700

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import mainLogo from "../assets/main-logo.png";
-
+import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer>
-       <Link to="/">
-            <span class="line"></span>
-                <img src={mainLogo} alt="35mm Logo" />
-                <span class="line"></span>
-            </Link>
-        <div className="footer-container">
-            <ul className="footer-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/favorites">Favourites</Link></li>
-            </ul>
-            <ul className="sub-menulinks">
-            <li><Link to="/now_playing">Now Playing</Link></li>
-            <li><Link to="/top_rated">Top Rated</Link></li>
-            <li><Link to="/upcoming">Upcoming</Link></li>
-            </ul>
-          
-            {/* <figure className="social-links">
+      <div className="footer-logo-container">
+        <span className="line"></span>
+        <Link to="/">
+          <img src={mainLogo} alt="35mm Logo" />
+        </Link>
+        <span className="line"></span>
+      </div>
+      <div className="footer-container">
+        <ul className="footer-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/favorites">Favourites</Link></li>
+          <li><Link to="/now_playing">Now Playing</Link></li>
+          <li><Link to="/top_rated">Top Rated</Link></li>
+          <li><Link to="/upcoming">Upcoming</Link></li>
+        </ul>
+        <figure className="social-links">
             <a href="https://discord.com">
               <img src={discordLogo} alt="Discord Logo" />
             </a>
@@ -33,9 +32,9 @@ const Footer = () => {
             <a href="https://youtube.com">
               <img src={youtubeLogo} alt="YouTube Logo" />
             </a>
-          </figure> */}
+          </figure>
+      </div>
             <p>Copyright © FWD 2024 Yining Li Mahdi Roozbahani Marc Sapa Kate Shepherd</p>
-        </div>
     </footer>
   );
 };
