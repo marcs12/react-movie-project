4a12da4b3be517ad495f949c60180bad1380c2cd
Edmarc Sapa <marcgsapa@gmail.com>
2024-08-24 16:19:41 -0700

    Search Checkpoint

--- src/globals/globalVariables.js
+++ src/globals/globalVariables.js
@@ -3,3 +3,4 @@ export const moviePoster = "https://image.tmdb.org/t/p/w500";
 export const apiKey = import.meta.env.VITE_MOVIE_API_KEY;
 export const API = `https://api.themoviedb.org/3`;
 export const movieAPI = `${API}/movie/popular?language=en-US&page=1&api_key=${apiKey}`;
+export const searchAPI = `${API}/search/movie?language=en-US&api_key=${apiKey}&query=`;
